a9b6830bf624c64ac81fc333b17802f7210c3241
DRGN-3455 <ts927@exeter.ac.uk>
2026-01-20 12:58:54 +0000

    Update event_planner.py
    Added a rough output scaffold to the event planner file

---
+++ Code/event_planner.py
@@ -0,0 +1,29 @@
+def final_output(input_file,time,budget):
+    # Base info Output
+    print("========================================")
+    print("EVENT PLANNER - RESULTS")
+    print("========================================")
+    print(f"Input File: {input}")
+    print(f"Available Time: {time} Hours")
+    print(f"Available Budget: £{budget}")
+    
+    # Brute Force Output
+    Print("--- BRUTE FORCE ALGORITHM ---")
+    Print("Selected Activities:")
+        # Output the list of activities from the brute force algorithm
+        # the list of activities from the brute force algorithm
+        # TOTAL ENJOYMENT
+        # TOTAL TIME USED:
+        # TOTAL COST
+        # TOTAL TIME TO EXECUTE
+    
+    # Dynamic Porgramming Output
+    Print("--- DYNAMIC PROGRAMMING ALGORITHM ---")
+    Print("Selected Activities:")
+        # Output
+        # the list of activities from the brute force algorithm
+        # TOTAL ENJOYMENT
+        # TOTAL COST
+        # TOTAL TIME TO EXECUTE
+        
+    Print("\n========================================")
