4fa5f8dbc1581e8cf4ba562cdfb8291b89688816
Henry Beedham <hb932@exeter.ac.uk>
2026-02-24 13:29:16 +0000

    seperate read_input_file

--- Code/event_planner.py
+++ Code/event_planner.py
@@ -4,11 +4,7 @@ import time
 from typing import List, Tuple
 import time
 
-# --- Ensure Python finds local modules ------------------------
-current_dir = os.path.dirname(os.path.abspath(__file__))
-if current_dir not in sys.path:
-    sys.path.append(current_dir)
-# --------------------------------------------------------------
+from read_input_file import read_input_file
 
 # --- Import your brute_force and dynamic programming modules here when implemented. Uncomment the lines below:
 from brute_force import brute_force_planner
@@ -26,33 +22,7 @@ class Activity:
 
   def __str__(self):
     return f"Activity: {self.name}, Duration: {self.duration} hours, Cost: £{self.cost}, Enjoyment Level: {self.enjoyment_level}"
-  
-def read_input_file(filename: str) -> Tuple[List[Activity], int, int]:
-  """Read csv file of input activities and constraints"""
-  try:
-    with open(filename, 'r') as f:
-      lines = f.readlines()
-    
-    n = int(lines[0].strip())
-    max_duration, max_budget = map(int, lines[1].strip().split())
-    
-    activities = []
-    for i in range(2, 2 + n):
-      parts = lines[i].strip().split()
-      name = parts[0]
-      duration = int(parts[1])
-      cost = int(parts[2])
-      enjoyment_level = int(parts[3])
-      activities.append(Activity(name, duration, cost, enjoyment_level))
-    
-    return activities, max_duration, max_budget
-      
-  except FileNotFoundError:
-      print(f"Error: File '{filename}' not found.")
-      sys.exit(1)
-  except Exception as e:
-      print(f"Error reading file: {e}")
-      sys.exit(1)
+
       
 def main():
   # Read filename from command line argument (e.g. python Code/event_planner.py ./Input_Files/input_small.txt)
---
+++ Code/read_input_file.py
@@ -0,0 +1,26 @@
+def read_input_file(filename: str) -> Tuple[List[Activity], int, int]:
+  """Read csv file of input activities and constraints"""
+  try:
+    with open(filename, 'r') as f:
+      lines = f.readlines()
+    
+    n = int(lines[0].strip())
+    max_duration, max_budget = map(int, lines[1].strip().split())
+    
+    activities = []
+    for i in range(2, 2 + n):
+      parts = lines[i].strip().split()
+      name = parts[0]
+      duration = int(parts[1])
+      cost = int(parts[2])
+      enjoyment_level = int(parts[3])
+      activities.append(Activity(name, duration, cost, enjoyment_level))
+    
+    return activities, max_duration, max_budget
+      
+  except FileNotFoundError:
+      print(f"Error: File '{filename}' not found.")
+      sys.exit(1)
+  except Exception as e:
+      print(f"Error reading file: {e}")
+      sys.exit(1)
