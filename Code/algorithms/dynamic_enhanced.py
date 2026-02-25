ef29fb9d810a64371eede68b11d5bcb002d40cd5
Henry Beedham <hb932@exeter.ac.uk>
2026-02-25 01:54:50 +0000

    Dynamic one constraint

--- Code/algorithms/dynamic_enhanced.py
+++ Code/algorithms/dynamic_enhanced.py
@@ -4,8 +4,8 @@ def dp_planner(activities, max_time, budget):
 
     n = len(activities)
 
-    # dp[t] store the maximum enjoyment level that can be achieved with up to t hours
-    dp = [0] * (budget + 1) 
+    # dp[t][b] store the maximum enjoyment level that can be achieved with up to t hours and b budget
+    dp = [[0] * (budget + 1) for _ in range(max_time + 1)]
     
     snapshots = []  # Save table state before adding each activity
     # to make it easier to figure out which activities were in the optimal solution
@@ -20,26 +20,27 @@ def dp_planner(activities, max_time, budget):
         
         # Go backwards to prevent using the same activity twice+ within one iteration
         for t in range(max_time, activity.duration - 1, -1):
-        
-            
-            # If we include this activity,
-            # calculate new enjoyment
-            candidate = dp[t - activity.duration] + activity.enjoyment_level
-            
-            # If including it gives better enjoyment,
-            # update the table
-            if candidate > dp[t]:
-                dp[t] = candidate
+            for b in range(budget, activity.cost - 1, -1):
+                
+                # If we include this activity,
+                # calculate new enjoyment
+                candidate = dp[t - activity.duration][b - activity.cost] + activity.enjoyment_level
+                
+                # If including it gives better enjoyment,
+                # update the table
+                if candidate > dp[t][b]:
+                    dp[t][b] = candidate
 
     # Now find best enjoyment value
     best_enjoyment = 0
     best_t = 0
     best_b = 0
     for t in range(max_time + 1):
-    
-        if dp[t] > best_enjoyment:
-            best_enjoyment = dp[t]
-            best_t = t
+        for b in range(budget + 1):
+            if dp[t][b] > best_enjoyment:
+                best_enjoyment = dp[t][b]
+                best_t = t
+                best_b = b
 
     # Now we traceback through the activities using the snapshots to find which caused a change in the optimal solution.
     selected_ids = []
@@ -48,10 +49,11 @@ def dp_planner(activities, max_time, budget):
     for i in range(n - 1, -1, -1):
         activity = activities[i]
         prev = snapshots[i]  # table state before activity i was added
-        if t >= activity.duration:
-            if dp[t] != prev[t]:  # activity i changed this cell => it was included
+        if t >= activity.duration and b >= activity.cost:
+            if dp[t][b] != prev[t][b]:  # activity i changed this cell => it was included
                 selected_ids.append(i)
                 t -= activity.duration
+                b -= activity.cost
         dp = prev  # roll back for the next iteration
 
     selected_ids = tuple(reversed(selected_ids))  # restore ascending order
