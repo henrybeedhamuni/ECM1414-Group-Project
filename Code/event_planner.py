4c6a2d015110f29d6455948c3b37f6eeb83c92ab
Henry Beedham <hb932@exeter.ac.uk>
2026-02-25 02:38:58 +0000

    Add enhanced dynamic to event planner

--- Code/event_planner.py
+++ Code/event_planner.py
@@ -11,6 +11,7 @@ from utils.read_input_file import read_input_file
 from algorithms.brute_force import brute_force_planner
 from algorithms.enhanced_brute_force import enhanced_brute_force_planner
 # from dynamic import dp_planner
+from algorithms.dynamic_enhanced import dp_planner as enhanced_dp_planner
 from algorithms.dynamic import dp_planner
 
 #Import Greedy Heuristic
@@ -44,7 +45,11 @@ def main():
   dp_output = dp_planner(activities, max_duration, max_budget)
   end = time.time()
   dp_output.append("{:.6f}".format(end-start))
-
+  
+  start = time.time()
+  enhanced_dp_output = enhanced_dp_planner(activities, max_duration, max_budget)
+  end = time.time()
+  enhanced_dp_output.append("{:.6f}".format(end-start))
   # --- End of dynamic programming usage
 
   # --- Begin Greedy Heuristic usage
@@ -52,10 +57,9 @@ def main():
   gh_output = greedy_heuristic(activities, max_duration, max_budget)
   end = time.time()
   gh_output.append("{:.6f}".format(end-start))
-
   # --- End of Greedy Heuristic  usage
   
-  solutions = [bf_output,bf_output_e,dp_output,gh_output]
+  solutions = [bf_output,bf_output_e,dp_output,enhanced_dp_output,gh_output]
   # Print read activities and constraints
   # returns[[solution],[cost],[time],[enjoyment],[execution time]]
   print("========================================")
@@ -85,12 +89,20 @@ def main():
       print(f"  {activities[index]}")
   print(f"Total Enjoyment: {solutions[2][3]}\nTotal Time Used: {solutions[2][2]} hours\nTotal Cost: £{solutions[2][1]}")
   print(f"\nExecution Time: {solutions[2][4]} Seconds")
-
-  print("\n\n --- Greedy Heuristic Algorithm --- ")
+  
+  
+  print("\n\n --- Enhanced Dynamic Programming Algorithm --- ")
   print("Selected Activities: ")
   for index in solutions[3][0]:
       print(f"  {activities[index]}")
   print(f"Total Enjoyment: {solutions[3][3]}\nTotal Time Used: {solutions[3][2]} hours\nTotal Cost: £{solutions[3][1]}")
   print(f"\nExecution Time: {solutions[3][4]} Seconds")
+
+  print("\n\n --- Greedy Heuristic Algorithm --- ")
+  print("Selected Activities: ")
+  for index in solutions[4][0]:
+      print(f"  {activities[index]}")
+  print(f"Total Enjoyment: {solutions[4][3]}\nTotal Time Used: {solutions[4][2]} hours\nTotal Cost: £{solutions[4][1]}")
+  print(f"\nExecution Time: {solutions[4][4]} Seconds")
 if __name__ == "__main__":
   main()
